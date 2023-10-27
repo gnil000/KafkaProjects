2333b02e2446518e0a7104f31fcace275f751145
GNIL <tony.vinz.one@gmail.com>
2023-10-27 13:12:26 +0300

    fff

--- KafkaConsumer/Program.cs
+++ KafkaConsumer/Program.cs
@@ -3,7 +3,7 @@ class Program
 {
 	static async Task Main(string[] args)
 	{
-		NewLibrary.Kafka.KafkaConsumer consumer = new NewLibrary.Kafka.KafkaConsumer("jsonTopic");
+		NewLibrary.Kafka.KafkaConsumer consumer = new NewLibrary.Kafka.KafkaConsumer("BaseTopic");
 		await consumer.StartAsync();
 	}
 }
